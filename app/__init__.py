132c6f0431ef4c5e9a0f35b6c78cc7aba546f5c7
NQTrung08 <nqtrung0810@gmail.com>
2024-05-14 13:32:59 +0700

    feat: get product_id && pagination

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,28 @@
 from flask import Flask, jsonify
 
-from app.controllers.product_controllers import get_products_controllers
-from app.models.database import get_database_connection
+from app.controllers.pagination_controllers import get_poduct_paginate
+from app.controllers.product_controllers import (
+    get_product_controllers,
+    get_products_controllers,
+)
 
 
 def init_app():
     app = Flask(__name__)
 
-    @app.route("/products")
+    @app.route("/products", methods=["GET"])
     def get_all_products():
         products = get_products_controllers()
+        return jsonify({"products": products})
+
+    @app.route("/product/<int:product_id>", methods=["GET"])
+    def get_product(product_id):
+        product = get_product_controllers(product_id)
+        return jsonify({"product": product})
+
+    @app.route("/sale", methods=["GET"])
+    def get_paginate():
+        products = get_poduct_paginate()
         return jsonify(products)
 
     return app
--- app/controllers/product_controllers.py
+++ app/controllers/product_controllers.py
@@ -7,11 +7,25 @@ def get_products_controllers():
     try:
         connection = get_database_connection()
         cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM product")
+        cursor.execute("SELECT * FROM products")
         products = cursor.fetchall()
         cursor.close()
         connection.close()
         return products
+    except mysql.connector.Error as err:
+        print("Error retrieving products:", err)
+        return None
+
+
+def get_product_controllers(product_id):
+    try:
+        connection = get_database_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM products WHERE id = {product_id}")
+        product_id = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return product_id
     except mysql.connector.Error as err:
         print("Error retrieving products:", err)
         return None
